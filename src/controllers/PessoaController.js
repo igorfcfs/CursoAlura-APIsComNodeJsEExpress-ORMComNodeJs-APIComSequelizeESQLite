8678baa42fa2e362cfc6fbd99c0cb14cf0e80090
igorfcfs <igorfcfs@gmail.com>
2024-07-26 16:53:36 -0300

    Atualizando a API

--- src/controllers/PessoaController.js
+++ src/controllers/PessoaController.js
@@ -3,10 +3,20 @@ const PessoaServices = require("../services/PessoaServices.js");
 
 const pessoaServices = new PessoaServices();
 
-class PessaController extends Controller {
+class PessoaController extends Controller {
   constructor(){
     super(pessoaServices);
   }
+
+  async pegaMatriculas(req, res){
+    const { estudante_id } = req.params;
+    try {
+      const listaMatriculas = await pessoaServices.pegaMatriculasPorEstudante(Number(estudante_id));
+      return res.status(200).json(listaMatriculas);
+    } catch (erro){
+      //erro
+    }
+  }
 }
 
-module.exports = PessaController;
+module.exports = PessoaController;
